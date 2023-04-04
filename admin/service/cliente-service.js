720698b8dd5d70cf299045903f6647913f2d434d
Franklin Souza <ffrancadesouza@gmail.com>
2023-04-04 17:31:08 -0300

    adicionando função de deletar clientes

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,10 +1,33 @@
 const listaDeClientes = () => {
-	return fetch("http://localhost:3000/profile").then((resposta) => {
+	return fetch(`http://localhost:3000/profile`).then((resposta) => {
 		resposta.json();
+	});
+};
+
+const criaCliente = (nome, email) => {
+	return fetch(`http://localhost:3000/profile`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			nome: nome,
+			email: email,
+		}),
+	}).then((resposta) => {
+		return resposta.body;
+	});
+};
+
+const removeCliente = (id) => {
+	return fetch(`http://localhost:3000/profile/${id}`, {
+		method: "DELETE",
 	});
 };
 
 const clienteService = {
 	listaDeClientes,
+	criaCliente,
+	removeCliente,
 };
 export default clienteService;
